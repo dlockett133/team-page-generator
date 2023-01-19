3aa75bb2dbd1b806c6b4ca0105309516b0316eb8
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-19 01:30:29 -0500

    modified Employee.js, and installed jest

---
+++ utils/employeeQuestions.js
@@ -0,0 +1,20 @@
+const questions = {
+    getName: {
+        type: `input`,
+        name: `employeeName`,
+        message: `What is the employee's name?`
+    },
+    
+    getID: {
+        type: `input`,
+        name: `id`,
+        message: `What is the employee's ID?`
+    },
+
+    getEmail: {
+        type: `input`,
+        name: `email`,
+        message: `What is the employee's email?`
+    }
+}
+module.exports = questions
