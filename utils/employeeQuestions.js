73220553ed030f939dcbc9cac682d31bf345581a
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-19 20:18:09 -0500

    Created questions for application

--- utils/employeeQuestions.js
+++ utils/employeeQuestions.js
@@ -1,20 +1,85 @@
-const questions = {
-    getName: {
+const managerQuestions = [
+    {
         type: `input`,
-        name: `employeeName`,
+        name: `managerName`,
+        message: `What is the team manager's name?`
+    },
+    
+    {
+        type: `input`,
+        name: `managerId`,
+        message: `What is the team manager employee's ID?`
+    },
+
+    {
+        type: `input`,
+        name: `managerEmail`,
+        message: `What is the team manager's email?`
+    },
+
+    {
+        type: `input`,
+        name: `officeNum`,
+        message: `What is the team's office number?`
+    },
+
+    {
+        type: `list`,
+        name: `addEmployee`,
+        message: `Would you like to:`,
+        choices: [`Add an engineer?`, `Add an intern?` `Or to finish building my team?`]
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: `input`,
+        name: `engineerName`,
         message: `What is the employee's name?`
     },
     
-    getID: {
+    {
         type: `input`,
-        name: `id`,
+        name: `engineerId`,
         message: `What is the employee's ID?`
     },
 
-    getEmail: {
+    {
         type: `input`,
-        name: `email`,
+        name: `engineerEmail`,
         message: `What is the employee's email?`
+    },
+
+    {
+        type: `input`,
+        name: `githubUsername`,
+        message: `What is the employee's GitHub username?`
+    },
+]
+
+const internQuestions = [
+    {
+        type: `input`,
+        name: `internName`,
+        message: `What is the employee's name?`
+    },
+    
+    {
+        type: `input`,
+        name: `internId`,
+        message: `What is the employee's ID?`
+    },
+
+    {
+        type: `input`,
+        name: `internEmail`,
+        message: `What is the employee's email?`
+    },
+
+    {
+        type: `input`,
+        name: `school`,
+        message: `What school does the intern attend?`
     }
-}
+]
 module.exports = questions
