5e4d6ca4423137808a03dd46abb121bc6dd70451
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-19 19:52:01 -0500

    started initial code for index.js, importing all classes and dependecies

---
+++ index.js
@@ -0,0 +1,16 @@
+// Going to use inquire to prompt manager.js variable for class arguments
+// then will ask if we would like to add an engineer or intern
+// then will use inquirer to get information from prompts to inter in either intern or engineer class
+// if chosen neither, it will exit out of script
+// will generate html after script is done
+
+
+
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+Manager
+Employee
+inquirer
