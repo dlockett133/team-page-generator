19278f0ae407ef5dbfe1255e368987d638c68477
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-21 02:20:07 -0500

    Created createTeam() function

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 let createTeam = false
+const myNewArray = []
 function init() {
     inquirer
         .prompt(questions.managerQuestions)
@@ -22,30 +23,55 @@ function init() {
             let id = data.managerId;
             let email = data.managerEmail;
             let officeNum = data.officeNum;
-            let addEmployee = data.addEmployee;
-
             const newManager = new Manager(name, id, email, officeNum);
-            console.log(newManager);
-            if (createTeam === false) {
-                if (addEmployee === `Add an engineer`){
-                    inquirer.prompt(questions.engineerQuestions)
-                        .then((data)=> {
-                            console.log(data)
-                            createTeam = true;
-                        });
-                } else if (addEmployee === `Add an intern`){
-                    inquirer.prompt(questions.internQuestions)
-                        .then((data)=> {
-                            console.log(data)
-                            createTeam = true;
-                        });
-                } else {
-                    createTeam = true;
-                }       
-            } else {
-                console.log("finish team")
-            }
+            // console.log(newManager);
+            myNewArray.push(newManager);
+            // console.log(myNewArray);
+            createTeam();
+            // console.log(myNewArray)
         })
+        // console.log("myNewArray")
+
+}
+
+function createTeam() {
+    inquirer
+    .prompt(questions.createTeam)
+    .then((data) => {
+        let addEmployee = data.addEmployee
+        console.log(addEmployee);
+        
+        if (createTeam === false) {
+            if (addEmployee === `Add an engineer`){
+                inquirer.prompt(questions.engineerQuestions)
+                    .then((data)=> {
+                        let name = data.engineerName;
+                        let id = data.engineerId;
+                        let email = data.engineerEmail;
+                        let github = data.githubUsername;
+                        const newEngineer = new Engineer(name, id, email, github);
+                        myNewArray.push(newEngineer)
+                        // console.log(myNewArray);
+                        createTeam();
+                    });
+            } else if (addEmployee === `Add an intern`){
+                inquirer.prompt(questions.internQuestions)
+                    .then((data)=> {
+                        let name = data.internName;
+                        let id = data.internId;
+                        let email = data.internEmail;
+                        let school = data.school;
+                        const newEIntern = new Engineer(name, id, email, github);
+                        myNewArray.push(newEIntern)
+                        // console.log(myNewArray);
+                        createTeam();
+                    });
+            } else {
+                createTeam = true;
+                console.log(myNewArray)
+            }       
+        } 
+    })
 }
 
 init();
